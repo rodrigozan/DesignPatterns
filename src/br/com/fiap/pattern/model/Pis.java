77bbcd0280633ddfbe2d6e09bc3a4cbcc9933f82
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2022-06-20 10:03:12 -0300

    Refactory model layer Imposto

--- src/br/com/fiap/pattern/model/Pis.java
+++ src/br/com/fiap/pattern/model/Pis.java
@@ -1,6 +1,9 @@
 package br.com.fiap.pattern.model;
 
-public class Pis implements Imposto {
+import java.util.Observable;
+
+// Observable significa que a classe está sendo observada pelos outros métodos
+public class Pis extends Observable implements Imposto {
 	
 	private float aliquota = 0.6f;
 	
@@ -8,8 +11,15 @@ public class Pis implements Imposto {
 
 	@Override
 	public void calcularImposto(float valor) {
-		// TODO Auto-generated method stub
 		
+		setChanged(); //avisa que haverá uma mudança
+		valorPis = valor * aliquota;
+		notifyObservers(getValorPis()); // notifica que houve uma mudança aqui
+		
+	}
+
+	public float getValorPis() {
+		return valorPis;
 	}
 
 }
